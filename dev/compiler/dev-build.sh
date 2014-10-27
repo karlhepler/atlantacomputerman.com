df1db12d6f777e4f3caa7790428d61f02a9f153a
Karl Hepler <karl.hepler@gmail.com>
2014-10-27 14:26:29 -0400

    Initial Commit

--- dev/compiler/dev-build.sh
+++
@@ -1,8 +0,0 @@
-# Compile the less file and outupt to dist.css
-lessc ../src/less/dev.less > ../../dist.css -x
-
-# Copy the index file
-cp -f ../index.html ../../index.html
-
-# Copy the pages
-cp -r -f ../pages/. ../../
