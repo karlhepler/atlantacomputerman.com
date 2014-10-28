b19282df876e09127ce79ef96a78f26752645730
Karl Hepler <karl.hepler@gmail.com>
2014-10-27 23:48:36 -0400

    Oh so many wonderful things

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,82 @@
 // JQUERY DOCUMENT READY --------------
 $(function() {
 
+	// Load fonts ---------------------------
+	WebFontConfig = {
+	    google: { families: [ 'Oswald:700', 'Voltaire', 'Bangers' ]
+	    },
+	    active: webFontsLoaded
+	  };
+	  (function() {
+	    var wf = document.createElement('script');
+	    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+	      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	    wf.type = 'text/javascript';
+	    wf.async = 'true';
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(wf, s);
+	  })();
+
+	// FONTS LOADED! DO STUFF!!!
+	function webFontsLoaded() {
+		$('.big-text').slabText();
+	}
+
 	// SKROLLR - init and disable on mobile
 	if( $(window).width() > 767 ) {
 		skrollr.init({
 			forceHeight: false
 		});
 	}
+
+	// Trigger modal when clicking quick support contact email link
+	$('.quick-support-contact-box .email:parent').click(function(e) {
+		e.preventDefault();
+
+		$('#quick-contact-modal').modal('show');
+	});
+
+	// Send Quick Contact Form
+	$('form.quick-contact').submit(function(e) {
+		e.preventDefault();
+		
+		// Serialize the form data
+		var formData = $(this).serialize();
+
+		// Start spinner
+		$('.spinner').show();
+
+		// Post the form
+		$.post('/quick-contact', formData, function(data) {
+			// Hide the spinner
+			$('.spinner').hide();
+			
+			// Close the modal
+			$('#quick-contact-modal').modal('hide');
+
+			// Clear the form
+			$(this)[0].reset();
+		});
+	});
+
+	// Send Contact Form
+	$('form.contact').submit(function(e) {
+		e.preventDefault();
+		
+		// Serialize the form data
+		var formData = $(this).serialize();
+
+		// Start spinner
+		$('.spinner').show();
+
+		// Post the form
+		$.post('/contact', formData, function(data) {
+			// Hide the spinner
+			$('.spinner').hide();
+
+			// Clear the form
+			$(this)[0].reset();
+		});
+	});
 	
 });
--- routes/index.js
+++ routes/index.js
@@ -41,3 +41,58 @@ exports.ourStory = function(req, res) {
 exports.signUp = function(req, res) {
 	res.render('sign-up', { title: 'Sign Up', menuItem: 'sign-up' });
 }
+
+exports.quickContact = function(req, res) {
+	Validator = require('validator.js');
+	var Assert = Validator.Assert,
+	    validator = new Validator.Validator();
+
+	var constraint = {
+		'subject': new Assert().NotBlank(),
+		'comment': new Assert().NotBlank(),
+		'name': new Assert().NotBlank(),
+		'email': new Assert().Email()
+	};
+
+	var isValid = validator.validate(req.body, constraint);
+
+	if ( isValid ) {
+		console.log(req.body);
+
+		// We have verified that all fields have been filled - now send to ZenDesk
+		var zendesk = require('node-zendesk'),
+		    fs      = require('fs');
+
+		var client = zendesk.createClient({
+		  username:  'atlantacomputerman@gmail.com',
+		  token:     '1Kb0XYfLXtCAXu3j29D1Uf3Oxomn2upcERGDWJer',
+		  remoteUri: 'https://atlantacomputerman.zendesk.com/api/v2'
+		});
+
+		var request = {
+			"ticket": {
+				"subject": req.body.subject,
+				"comment": { "body": req.body.comment },
+				"requester": { "locale_id": 1, "name": req.body.name, "email": req.body.email }
+			}
+		};
+
+		client.tickets.create(request, function(zen_err, zen_req, zen_res ) {
+			if (zen_err) {
+				res.send(zen_err);
+				return;
+			}
+			else {
+				res.send(zen_res);
+				return;
+			}
+		});
+	}
+	else {
+		res.send( isValid );
+	}
+}
+
+exports.contact = function(req, res) {
+
+}
